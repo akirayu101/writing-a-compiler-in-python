1ff740253e79df1bc53a4d9acc3e352907000581
akirayu101 <akirayu101@gmail.com>
2015-03-20 13:52:52 +0800

    init commit for empty main compiler

---
+++ 1/main.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+
+
+class Compiler(object):
+
+    @staticmethod
+    def compile(exp):
+        print(r'''
+	.section	__TEXT,__text,regular,pure_instructions
+	.globl	_main
+	.align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp2:
+	.cfi_def_cfa_offset 16
+Ltmp3:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp4:
+	.cfi_def_cfa_register %rbp
+	movl	$0, %eax
+	popq	%rbp
+	retq
+	.cfi_endproc
+.subsections_via_symbols
+        ''')
+
+
+Compiler.compile(None)
